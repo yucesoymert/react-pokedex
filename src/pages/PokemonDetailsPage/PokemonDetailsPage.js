2fcaebc7cd3f01ef7462e47db1796d7d78d681e6
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-08-20 22:05:13 +0300

    updated navigation structure

--- src/pages/PokemonDetailsPage/PokemonDetailsPage.js
+++ src/pages/PokemonDetailsPage/PokemonDetailsPage.js
@@ -1,12 +1,16 @@
 import './PokemonDetailsPage.css'
+import { useState } from "react";
 import { useNavigate, NavLink, Outlet } from 'react-router-dom'
 
 //components
 import AvatarCard from '../../component/DetailsPageTab/AvatarCard/AvatarCard';
+import { Biography } from '../../component/DetailsPageTab/Biography/Biography';
 
 const PokemonDetailsPage = () => {
 
     let navigate = useNavigate();
+
+    const [section, setSection] = useState("biography")
 
     return (
         <div className='detail-page'>
@@ -18,10 +22,13 @@ const PokemonDetailsPage = () => {
                 <div className='info-container'>
                     <div className='right-side'>
                         <nav className='nav-bar'>
-                            <NavLink className="nav-item" to="biography">Biography</NavLink>
-                            <NavLink className="nav-item" to="stats">Stats</NavLink>
-                            <NavLink className="nav-item" to="evolutions">Evolutions</NavLink>
+                            <NavLink className="nav-item " onClick={() => setSection("stats")} to="biography" >Biography</NavLink>
+                            <NavLink className="nav-item" onClick={() => setSection("stats")} to="stats">Stats</NavLink>
+                            <NavLink className="nav-item" onClick={() => setSection("evolutions")} to="evolutions">Evolutions</NavLink>
                         </nav>
+                        <div>
+                            {(section === "biography") ? <Biography /> : ""  }  
+                        </div>
                         <Outlet />
                     </div>
                 </div>
