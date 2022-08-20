a4f1482e4a60902d681ee4abf1c2e3b8530521ab
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-08-20 07:06:30 +0300

    updated class name in header component

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,17 +1,16 @@
-import pokeball from 'C:/Users/merty/bootcamp/react-pokedex/src/svg/pokeball.svg';
+import pokeball from '../../images/svg/pokeball.svg';
 import './Header.css'
 
 import { useNavigate } from 'react-router-dom';
-
 
 export const Header = () => {
     let navigate = useNavigate();
     return (
         <div className="header">
-            <div className="header__btn" onClick={() => {
+            <div className="header-btn" onClick={() => {
                     navigate("/");
                 }}>
-                <img src={pokeball} className="pokeball__logo" alt="logo" />
+                <img src={pokeball} className="pokeball-logo" alt="logo" />
             </div>
         </div>
     )
