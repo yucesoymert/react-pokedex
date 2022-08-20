cfaeedeb4e274ee272275e77325d5590e00b698f
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-08-20 06:59:34 +0300

    updated index.js router line

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//style
 import './index.css';
+import './component/Header/Header.css'
+
 import App from './App';
+
+//components
+import { Header } from './component/Header/Header';
+import PokemonDetailsPage from './pages/PokemonDetailsPage/PokemonDetailsPage';
+import { Biography } from './component/DetailsPageTab/Biography/Biography';
+import { Stats } from './component/DetailsPageTab/Stats/Stats';
+import { Evolutions } from './component/DetailsPageTab/Evolutions/Evolutions';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Header />
+    <Routes className="pokemon-container">
+      <Route path="/" element={<App />}></Route>
+      <Route path="/details/:id/*" element={<PokemonDetailsPage />}>
+        <Route path="biography" element={<Biography />} />
+        <Route path="stats" element={<Stats />} />
+        <Route path="evolutions" element={<Evolutions />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
