68ba6e8fc29846e0175fe0b15870118a1edeb90f
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-08-20 07:05:23 +0300

    fixed console error and gender type value in biography component

--- src/component/DetailsPageTab/Biography/Biography.js
+++ src/component/DetailsPageTab/Biography/Biography.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Biography.css"
 
+import male from '../../../images/png/male.png';
+import female from '../../../images/png/female.png';
+
 import { useParams } from 'react-router-dom';
 import { usePokemon } from "../../../hooks/usePokemon";
 import { usePokemonSpecies } from '../../../hooks/usePokemonSpecies';
@@ -11,7 +14,7 @@ export const Biography = () => {
     const pokemon = usePokemon(id)
     const pokemonSpecies  = usePokemonSpecies(id)
 
-    // const genderPercentage = pokemonSpecies.gender_rate !== -1 ? (pokemonSpecies.gender_rate / 8) * 100 : -1;
+    const genderPercentage = pokemonSpecies?.gender_rate !== -1 ? (pokemonSpecies?.gender_rate / 8) * 100 : -1;
 
     if (!pokemon) {
         return null;
@@ -22,12 +25,12 @@ export const Biography = () => {
             <div className="pokemon-data">
                 <div className="pokemon-text">
                 <h2 className="title">Pokémon Data</h2>
-                    <div className="definition">{pokemonSpecies.flavor_text_entries[2].flavor_text}</div>
+                    <div className="definition">{pokemonSpecies?.flavor_text_entries[2]?.flavor_text}</div>
                 </div>
                 <div className="data-container">
                     <div className="grid-context">
                         <div className="stat-name">Species</div>
-                        <div className="stat-value">{pokemonSpecies.genera[3].genus}</div>
+                        <div className="stat-value">{pokemonSpecies?.genera[3]?.genus}</div>
                     </div>
                     <div className="grid-context">
                         <div className="stat-name">Weight</div>
@@ -49,25 +52,25 @@ export const Biography = () => {
                         <div className="stat-name">Gender</div>
                         <div className="stat-value">
                             <div className="gender-types">
-                                {15 === -1 ? (
+                                {genderPercentage === -1 ? (
                                 <span>Genderless</span>
                                 ) : (
                                 <>
                                 <div className="gender-type">
-                                    {/* <img
-                                        className="male-icon"
-                                        src="./public/assets/images/male.png"
+                                    <img
+                                        className="gender-icon"
+                                        src={male}
                                         alt="male"
-                                    /> */}
-                                    <div>{100 - 15}%</div>
+                                    />
+                                    <div>{100 - genderPercentage}%</div>
                                 </div>
                                 <div className="gender-type">
-                                    {/* <img
-                                        className="female-icon"
-                                        src="./public/assets/images/female.png"
+                                    <img
+                                        className="gender-icon"
+                                        src={female}
                                         alt="female"
-                                    /> */}
-                                    <div>{15}%</div>
+                                    />
+                                    <div>{genderPercentage}%</div>
                                 </div>
                                 </>
                                 )}
@@ -81,19 +84,19 @@ export const Biography = () => {
                 <div className="data-container">
                     <div className="grid-context">
                         <div className="stat-name">Base Exp</div>
-                        <div className="stat-value">{pokemon.base_experience}</div>
+                        <div className="stat-value">{pokemon?.base_experience}</div>
                     </div>
                     <div className="grid-context">
                         <div className="stat-name">Base Happiness</div>
-                        <div className="stat-value">{pokemonSpecies.base_happiness}</div>
+                        <div className="stat-value">{pokemonSpecies?.base_happiness}</div>
                     </div>
                     <div className="grid-context">
                         <div className="stat-name">Catch Rate</div>
-                        <div className="stat-value">{(((pokemonSpecies.capture_rate) / 255) * 100).toFixed(1)}%</div>
+                        <div className="stat-value">{(((pokemonSpecies?.capture_rate) / 255) * 100).toFixed(1)}%</div>
                     </div>
                     <div className="grid-context">
                         <div className="stat-name">Growth Rate</div>
-                        <div className="stat-value">{pokemonSpecies.growth_rate.name}</div>
+                        <div className="stat-value">{pokemonSpecies?.growth_rate.name}</div>
                     </div>
                 </div>
             </div>
