dc6a201ba4298c24b19238f04a4ed485d1e74b95
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-08-20 07:13:26 +0300

    updated router line in pokemondetailspage

--- src/pages/PokemonDetailsPage/PokemonDetailsPage.js
+++ src/pages/PokemonDetailsPage/PokemonDetailsPage.js
@@ -1,38 +1,28 @@
 import './PokemonDetailsPage.css'
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
-
-import { useParams } from 'react-router-dom';
-import { usePokemon } from '../../hooks/usePokemon';
-import { usePokemonSpecies } from '../../hooks/usePokemonSpecies';
+import { useNavigate, NavLink, Outlet } from 'react-router-dom'
 
 //components
 import AvatarCard from '../../component/DetailsPageTab/AvatarCard/AvatarCard';
-import { Biography } from '../../component/DetailsPageTab/Biography/Biography';
-import { Stats } from '../../component/DetailsPageTab/Stats/Stats';
-import { Evolutions } from '../../component/DetailsPageTab/Evolutions/Evolutions';
 
 const PokemonDetailsPage = () => {
-    
-    const {id} = useParams()
-    const pokemon = usePokemon(id)
-    const pokemonSpecies  = usePokemonSpecies(id)
+
+    let navigate = useNavigate();
 
     return (
         <div className='detail-page'>
+            <div className="back-btn">
+                <button type="button" onClick={() => navigate("/")}>Go Back</button>
+            </div>
             <div className='detail-card'>
                 <AvatarCard />
-                <div className='right-side'>
-                    <nav className='nav-bar'>
-                        <NavLink className="nav-item" to="biography">Biography</NavLink>
-                        <NavLink className="nav-item" to="stats">Stats</NavLink>
-                        <NavLink className="nav-item" to="evolutions">Evolutions</NavLink>
-                    </nav>
-                    <div className='info-container'>
-                        <Routes>
-                            <Route path="biography" element={<Biography />}/>
-                            <Route path="stats" element={<Stats />}/>
-                            <Route path="evolutions" element={<Evolutions />}/>
-                        </Routes>
+                <div className='info-container'>
+                    <div className='right-side'>
+                        <nav className='nav-bar'>
+                            <NavLink className="nav-item" to="biography">Biography</NavLink>
+                            <NavLink className="nav-item" to="stats">Stats</NavLink>
+                            <NavLink className="nav-item" to="evolutions">Evolutions</NavLink>
+                        </nav>
+                        <Outlet />
                     </div>
                 </div>
             </div>
