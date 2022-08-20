1939f55c01357b8908f767f58ae0b9f912a30dd1
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-08-20 06:59:10 +0300

    updated app.js router line

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 //pages
 import HomePage from './pages/HomePage/HomePage';
-import PokemonDetailsPage from './pages/PokemonDetailsPage/PokemonDetailsPage';
-import ErrorPage from './pages/ErrorPage/ErrorPage';
-
-//components
-import { Header } from './component/Header/Header';
 
 function App() {
   return (
-    <Router className="app-container">
-      <Header />
-      <Routes className="pokemon-container">
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/details/:id/*" element={<PokemonDetailsPage />}/>
-        {/* <Route path="*" element={<ErrorPage />}/> */}
-      </Routes>
-    </Router>
+      <HomePage />
   );
 }
 
