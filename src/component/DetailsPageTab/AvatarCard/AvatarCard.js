95096847e592fb170b6348e8c1c4cf24ddae4ce4
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-08-20 07:03:56 +0300

    updated by importing constant value in avatar card components

--- src/component/DetailsPageTab/AvatarCard/AvatarCard.js
+++ src/component/DetailsPageTab/AvatarCard/AvatarCard.js
@@ -3,29 +3,7 @@ import './AvatarCard.css'
 import { useParams } from 'react-router-dom';
 import { usePokemon } from '../../../hooks/usePokemon';
 import { usePokemonSpecies } from '../../../hooks/usePokemonSpecies';
-
-const IMAGE_URL = "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/"
-
-const typeColors = {
-    grass: "rgb(138, 209, 102)",
-    poison: "rgb(160, 64, 160)",
-    fire: "rgb(245, 158, 95)",
-    water: "rgb(129, 207, 224)",
-    bug: "rgb(168, 184, 32)",
-    normal: "rgb(196, 196, 164)",
-    electric: "rgb(248, 208, 48)",
-    ground: "rgb(224, 192, 104)",
-    fairy: "rgb(238, 153, 172)",
-    fighting: "rgb(204, 67, 59)",
-    ice: "rgb(107, 192, 209)",
-    flying: "rgb(255, 148, 112)",
-    psychic: "rgb(248, 88, 136)",
-    rock: "rgb(184, 160, 56)",
-    ghost: "rgb(236, 178, 247)",
-    dragon: "rgb(112, 56, 248)",
-    dark: "rgb(112, 88, 72)",
-    steel: "rgb(184, 184, 208)"
-}
+import { POKEMON_IMAGE_URL, POKEMON_TYPE_TO_COLOR } from '../../../global/constants';
 
 const AvatarCard = () => {
 
@@ -40,10 +18,7 @@ const AvatarCard = () => {
     }
 
     const firstType = pokemon.types[0];
-    const typeColor = typeColors[firstType.type.name] ? typeColors[firstType.type.name]: "white";
-
-    const pokemonName = pokemon.name;
-    const newPokemonName = pokemonName[0].toUpperCase() + pokemonName.substring(1);
+    const typeColor = POKEMON_TYPE_TO_COLOR[firstType.type.name] ? POKEMON_TYPE_TO_COLOR[firstType.type.name]: "white";
 
     return (
         <div className='left-side'>
@@ -54,17 +29,17 @@ const AvatarCard = () => {
                     <div className="pokemon-number">
                         #{imageId}
                     </div>
-                    <div className="pokemon-nam">
-                        {newPokemonName}
+                    <div className="poke-name">
+                        {pokemon.name}
                     </div>
                 </div>
                 <div className="middle-card">
                     <div className='name-other'>
-                        {pokemonSpecies.names[0].name}
+                        {pokemonSpecies?.names[0]?.name}
                     </div>
                     <div className='pokemon-pictures'>
                         <div className='circle'></div>
-                        <img src={`${IMAGE_URL}${imageId}.png`}/>
+                        <img src={`${POKEMON_IMAGE_URL}${imageId}.png`} alt="pokemon_picture"/>
                     </div>
                 </div>
                 <div className="pokemon-types">
